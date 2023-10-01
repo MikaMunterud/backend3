31d7ca5453a0d40476704b5e147a6b1a0fc694c1
sofiaCoder <sofia.laas@gmail.com>
2023-10-01 23:00:02 +0200

    Skeleton of a POST Store API created

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -2,4 +2,11 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
-export async function POST() {}
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { name } = body;
+  
+  const store = await prismadb.store.create({ data: { name }})
+
+  return NextResponse.json(store);
+}
