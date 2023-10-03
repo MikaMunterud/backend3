2eb9c319f60a570c5186a9dd021bec3b9337887a
sofiaCoder <sofia.laas@gmail.com>
2023-10-03 16:33:59 +0200

    GET product by Id created

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -1,9 +1,35 @@
 import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+import { number } from 'zod';
 
-export async function GET() {}
+export async function GET(req: Request, { params }: { params: {productId: string }}) {
+  try {
+    const { userId } = auth()
 
-export async function DELETE() {}
+    if(userId) {
+      return new NextResponse('Not authorized', { status: 401 })
+    }
+
+    if(!params.productId) {
+      return new NextResponse('productId is required', { status: 400 })
+    }
+    
+    const result = await prismadb.product.findUnique({ where: { id: params.productId}})
+
+    if(result === null){
+      return new NextResponse('No product found', { status: 404 })
+    }
+    return NextResponse.json(result, { status: 200 })
+
+  } catch (error) {
+    return NextResponse.json({error}, {status: 500})
+  }
+}
+
+export async function DELETE(req: Request, { params }: { params: {productId: string}}) {
+  id: number;
+  const result = await prismadb.product.delete({ where: { id: params.productId}})
+}
 
 export async function PATCH() {}
