e857bc28b9bed0d96d227da6a7665ea63cd2b889
sofiaCoder <sofia.laas@gmail.com>
2023-10-03 20:25:42 +0200

    PATCH product created

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -49,4 +49,31 @@ export async function DELETE(req: Request, { params }: { params: {productId: str
   }
 }
 
-export async function PATCH() {}
+export async function PATCH(req: Request, { params }: { params: {productId: string}}) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name, img } = body;
+
+    if(!userId) {
+      return new NextResponse('Not authorized', { status: 401 })
+    }
+
+    if(!params.productId) {
+      return new NextResponse('productId is required', { status: 400 });
+    }
+
+    const result = await prismadb.product.update({ 
+      where: { id: params.productId 
+      },
+      data: {
+        name: name,
+        img: img
+      }
+    })
+    return NextResponse.json(result, { status: 200})
+  } catch (error) {
+    return NextResponse.json({error}, {status: 500});
+  }
+}
