f1aa9f5ae159fd7939fb5524d6e012afb8f4b389
Mika Munterud <mika.munterud@outlook.com>
2023-10-06 22:18:52 +0200

    Replacing initial size table with DataTable component and fetching data from api route

--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,27 +1,41 @@
-import { CellAction } from '@/components/cell-actions';
-import ApiList from '@/components/ui/api-list';
-import { Button } from '@/components/ui/button';
+'use client';
+
 import Heading from '@/components/ui/heading';
-import { Input } from '@/components/ui/input';
-import { Separator } from '@/components/ui/separator';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
+
+import { SizeColumn, columns } from './components/columns';
+import { DataTable } from '@/components/ui/data-table';
+
+import ApiList from '@/components/ui/api-list';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
 
 export default function Sizes() {
-  const sizesAmount: number = 1;
+  const [sizes, setSizes] = useState<SizeColumn[]>([]);
+  const params = useParams();
+
+  useEffect(
+    function () {
+      async function getSizes() {
+        const response = await axios.get(`/api/${params.storeId}/sizes`);
+        const data = await response.data.body.result;
+
+        setSizes(data);
+      }
+      getSizes();
+    },
+    [params.storeId],
+  );
 
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Sizes (${sizesAmount})`}
+          title={`Sizes (${sizes.length})`}
           description="Manage sizes for your store"
         />
 
@@ -30,41 +44,14 @@ export default function Sizes() {
         </Button>
       </div>
       <Separator />
-      <div>
-        <div className="flex items-center py-4">
-          <Input className="max-w-sm" placeholder="Search" />
-        </div>
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>Value</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Value</TableCell>
-                <TableCell>September 28th, 2023</TableCell>
-                <TableCell>
-                  <CellAction route={'sizes'} id={'dataID'} />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button className="h-10  px-4 py-2" variant="outline" disabled>
-            Previous
-          </Button>
-          <Button className="h-10  px-4 py-2" variant="outline" disabled>
-            Next
-          </Button>
-        </div>
-      </div>
+
+      <DataTable
+        searchKey="name"
+        columns={columns}
+        data={sizes}
+        route="sizes"
+      />
+
       <Heading title="API Routes" description="Endpoints for sizes" />
       <Separator />
       <ApiList entityName="sizes" entityIdName="sizeId" />
