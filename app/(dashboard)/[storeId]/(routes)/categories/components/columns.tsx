b97a5866c464c27fc379533c9379ca9d5c3896da
Mika Munterud <mika.munterud@outlook.com>
2023-10-11 16:31:47 +0200

    Fixing cell-action route to categories

--- app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import { CellAction } from "@/components/cell-actions";
-import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from '@/components/cell-actions';
+import { ColumnDef } from '@tanstack/react-table';
 
 export type CategoryColumn = {
   id: string;
   name: string;
-  billboardLabel: string;
+  billboardId: string;
 };
 
 export const columns: ColumnDef<CategoryColumn>[] = [
   {
-    accessorKey: "name",
-    header: "Name",
+    accessorKey: 'name',
+    header: 'Name',
   },
   {
-    accessorKey: "billboard",
-    header: "Billboard",
+    accessorKey: 'billboardId',
+    header: 'Billboard',
   },
   {
-    id: "actions",
-    cell: ({ row }) => <CellAction route={"sizes"} id={row.original.id} />,
+    id: 'actions',
+    cell: ({ row }) => <CellAction route={'categories'} id={row.original.id} />,
   },
 ];
