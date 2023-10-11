c40052793b24bf9b2c45bb3706316b99c8a7cac3
Mika Munterud <mika.munterud@outlook.com>
2023-10-11 17:24:33 +0200

    Adding initial sizeId GET to work in the update form on dashboard

--- app/api/[storeId]/sizes/[sizeId]/route.ts
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -1,61 +1,82 @@
 import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
+import { PrismaClientValidationError } from '@prisma/client/runtime/library';
 import { NextResponse } from 'next/server';
 
-export async function GET() { }
+export async function GET(
+  req: Request,
+  { params }: { params: { sizeId: string } },
+) {
+  try {
+    const { sizeId } = params;
+
+    const size = await prismadb.size.findUnique({
+      where: {
+        id: sizeId,
+      },
+    });
+
+    return NextResponse.json(size, { status: 200 });
+  } catch (err) {
+    if (err instanceof PrismaClientValidationError) {
+      const errorMessage = err.message;
+
+      return NextResponse.json({ status: 400, body: { errorMessage } });
+    } else {
+      return NextResponse.json({ status: 500, body: { err } });
+    }
+  }
+}
 
 export async function PATCH(
-    req: Request,
-    { params }: { params: { sizeId: string } },
+  req: Request,
+  { params }: { params: { sizeId: string } },
 ) {
-    /*     const { userId } = auth()
+  /*     const { userId } = auth()
     
         if (!userId) {
             return NextResponse.json({ error: 'Not authorized' }, { status: 401 })
         } */
 
-    try {
-        const body = await req.json()
-        const { sizeId } = params
+  try {
+    const body = await req.json();
+    const { sizeId } = params;
 
-        const res = await prismadb.size.update({
-            where: { id: sizeId },
-            data: {
-                name: body.name,
-                value: body.value
-            },
-        })
-        return NextResponse.json(`${res.name} is now updated`, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({ error }, { status: 500 })
-    }
-
+    const res = await prismadb.size.update({
+      where: { id: sizeId },
+      data: {
+        name: body.name,
+        value: body.value,
+      },
+    });
+    return NextResponse.json(`${res.name} is now updated`, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
 
 export async function DELETE(
-    req: Request,
-    { params }: { params: { sizeId: string } },
+  req: Request,
+  { params }: { params: { sizeId: string } },
 ) {
-    try {
-        /*         const { userId } = auth()
+  try {
+    /*         const { userId } = auth()
         
                 if (!userId) {
                     return NextResponse.json({ error: 'Not authorized' }, { status: 401 })
                 } */
 
-        if (!params.sizeId) {
-            return new NextResponse('sizeId required', { status: 400 });
-        }
-
-        const { sizeId } = params;
-
-        const res = await prismadb.size.delete({
-            where: { id: sizeId },
-        })
-        return NextResponse.json(`${res.name} is now deleted`, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({ error }, { status: 500 })
-
+    if (!params.sizeId) {
+      return new NextResponse('sizeId required', { status: 400 });
     }
-}
 
+    const { sizeId } = params;
+
+    const res = await prismadb.size.delete({
+      where: { id: sizeId },
+    });
+    return NextResponse.json(`${res.name} is now deleted`, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
