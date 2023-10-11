006195941d6fe65ece951fcc8356567859b8a968
elinhorn <elin.horn@hyperisland.se>
2023-10-11 10:33:53 +0200

    created a delete endpoint for size :tada:

--- app/api/[storeId]/sizes/[sizeId]/route.ts
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -2,8 +2,33 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
-export async function GET() {}
+export async function GET() { }
 
-export async function DELETE() {}
+export async function DELETE(
+    req: Request,
+    { params }: { params: { sizeId: string } },
+) {
+    try {
+        /*         const { userId } = auth()
+        
+                if (!userId) {
+                    return NextResponse.json({ error: 'Not authorized' }, { status: 401 })
+                } */
 
-export async function PATCH() {}
+        if (!params.sizeId) {
+            return new NextResponse('sizeId required', { status: 400 });
+        }
+
+        const { sizeId } = params;
+
+        const res = await prismadb.size.delete({
+            where: { id: sizeId },
+        })
+        return NextResponse.json(`${res.name} is now deleted`, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 })
+
+    }
+}
+
+export async function PATCH() { }
