f99eefe4a3699b25535f2a8477c51ed239ab1438
elinhorn <elin.horn@hyperisland.se>
2023-10-11 14:10:50 +0200

    created a PATCH endpoint for size :tada:

--- app/api/[storeId]/sizes/[sizeId]/route.ts
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -2,8 +2,34 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
-export async function GET() {}
+export async function GET() { }
 
-export async function DELETE() {}
+export async function DELETE() { }
 
-export async function PATCH() {}
+export async function PATCH(
+    req: Request,
+    { params }: { params: { sizeId: string } },
+) {
+    /*     const { userId } = auth()
+    
+        if (!userId) {
+            return NextResponse.json({ error: 'Not authorized' }, { status: 401 })
+        } */
+
+    try {
+        const body = await req.json()
+        const { sizeId } = params
+
+        const res = await prismadb.size.update({
+            where: { id: sizeId },
+            data: {
+                name: body.name,
+                value: body.value
+            },
+        })
+        return NextResponse.json(`${res.name} is now updated`, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 })
+    }
+
+}
