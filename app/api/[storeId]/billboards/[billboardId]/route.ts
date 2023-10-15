eb437c24a9a64c5e6fde3112a44223f6b3bf18e0
Mika Munterud <mika.munterud@outlook.com>
2023-10-15 21:08:34 +0200

    Adding GET, PATCH and DELETE for billboardId

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -2,8 +2,85 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
-export async function GET() {}
+export async function GET(
+  req: Request,
+  { params }: { params: { billboardId: string } },
+) {
+  try {
+    if (!params.billboardId) {
+      return new NextResponse('BillboardId is required', { status: 400 });
+    }
 
-export async function DELETE() {}
+    const result = await prismadb.billboard.findUnique({
+      where: { id: params.billboardId },
+    });
 
-export async function PATCH() {}
+    if (result === null) {
+      return new NextResponse('No product found', { status: 404 });
+    }
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { billboardId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Not authorized', { status: 401 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse('BillboardId is required', { status: 400 });
+    }
+
+    const result = await prismadb.billboard.delete({
+      where: { id: params.billboardId },
+    });
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { billboardId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Not authorized', { status: 401 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse('BillboardId is required', { status: 400 });
+    }
+
+    interface Body {
+      name: string;
+      image: string;
+    }
+
+    const { name, image }: Body = await request.json();
+
+    const result = await prismadb.billboard.update({
+      where: { id: params.billboardId },
+      data: {
+        name,
+        image,
+      },
+    });
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
