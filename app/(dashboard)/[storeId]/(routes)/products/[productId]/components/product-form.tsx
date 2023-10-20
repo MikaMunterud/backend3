9be3279187436967dcc249cfa32624a3341c75b5
Mika Munterud <mika.munterud@outlook.com>
2023-10-21 00:06:24 +0200

    Fixing product  description length

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -45,7 +45,7 @@ const formSchema = z.object({
   description: z
     .string()
     .min(1, { message: 'Description must be at least one character.' })
-    .max(500, { message: 'Description must be less than 500 characters.' }),
+    .max(200, { message: 'Description must be less than 200 characters.' }),
   price: z.coerce
     .number()
     .min(0.1, { message: 'Price is required' })
