8a8f44f95958bd250f58940f6fa942d56e67aa77
sofiaCoder <sofia.laas@gmail.com>
2023-10-20 12:03:09 +0200

    feat/GET orders by orderId added

--- app/api/[storeId]/orders/[orderId]/route.ts
+++ app/api/[storeId]/orders/[orderId]/route.ts
@@ -3,6 +3,40 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
+export async function GET(
+  request: Request,
+  { params }: { params: { storeId: string; orderId: string } }
+){
+  try {
+    const { storeId } = params;
+
+    if (!storeId) {
+      return NextResponse.json({ error: "StoreId is required.", status: 400 });
+    }
+
+    const { orderId } = params;
+
+    if (!orderId) {
+      return NextResponse.json({ error: "OrderId is required.", status: 400 });
+    }
+
+    const orders = await prismadb.order.findMany({
+      where: {
+        id: orderId
+      }
+    })
+
+    if (orders.length === 0){
+      return NextResponse.json({ error: "No orders were found.", status: 400 });
+    }
+
+    return NextResponse.json(orders, { status: 200 });
+  } catch (error){
+    return NextResponse.json(error, { status: 500 });
+  }
+}
+
+
 export async function PATCH(
   request: Request,
   { params }: { params: { storeId: string; orderId: string } }
